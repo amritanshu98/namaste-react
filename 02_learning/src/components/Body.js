7154b3b37198c85eb3068f36a7518d02a0bc32b2
amritanshu98 <amrit0498@gmail.com>
2024-10-14 03:24:22 +0530

    Episode 5

--- 02_learning/src/components/Body.js
+++ 02_learning/src/components/Body.js
@@ -1,16 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" onClick={() => {}}>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+            console.log(filteredList);
+          }}
+        >
           Popular Restaurants
         </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
--- 02_learning/src/components/Header.js
+++ 02_learning/src/components/Header.js
@@ -8,10 +8,18 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+          <li>
+            <a href="#">Cart</a>
+          </li>
         </ul>
       </div>
     </div>
